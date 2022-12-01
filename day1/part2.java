21972d3123474cf52766740af22b46614e74cf22
Andre Lacroix <andre1048576@gmail.com>
2022-12-01 09:35:56 -0500

    removed printing every line

--- day1/part2.java
+++ day1/part2.java
@@ -10,9 +10,6 @@ public class part2 {
         if (!part2.class.getPackage().getName().equals("day"+Integer.toString(day_number))) {
             System.out.println("WRONG DAY_NUMBER ENTERED");
         }
-        for (String l : lines) {
-            System.out.println(l);
-        }
         solve(lines);
     }
 
